80b1e64fd334f4b10fcb9518de1fdfa3b79bec12
Arjun <arjunsah856@gmail.com>
2025-08-13 14:25:51 +0545

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import { Button } from "@/components/ui/button"
+
+function App() {
+
+  return (
+    <>
+    <h1 className="container text-3xl font-bold text-center">
+    Hello world!
+  </h1>
+  <Button variant="outline">Button</Button>
+  </>
+  )
+}
+
+export default App
